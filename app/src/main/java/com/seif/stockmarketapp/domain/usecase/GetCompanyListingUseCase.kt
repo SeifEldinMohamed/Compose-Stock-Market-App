816634718cfd3b1f36fa63f11ce53b5958994d16
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-10-01 13:09:19 +0200

    GetCompanyListingUseCase Added

---
+++ app/src/main/java/com/seif/stockmarketapp/domain/usecase/GetCompanyListingUseCase.kt
@@ -0,0 +1,16 @@
+package com.seif.stockmarketapp.domain.usecase
+
+import com.seif.stockmarketapp.domain.model.CompanyListing
+import com.seif.stockmarketapp.domain.repository.StockRepository
+import com.seif.stockmarketapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetCompanyListingUseCase @Inject constructor(
+    private val stockRepository: StockRepository
+) {
+    suspend operator fun invoke(shouldFetch: Boolean = true, query: String):
+            Flow<Resource<List<CompanyListing>>> {
+        return stockRepository.getCompanyListings(shouldFetch, query)
+    }
+}
