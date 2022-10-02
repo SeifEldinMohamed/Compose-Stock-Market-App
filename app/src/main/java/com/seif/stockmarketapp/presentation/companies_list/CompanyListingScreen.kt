8809f64b488746240a60b3eb2507c17d675fa89b
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-10-02 11:44:12 +0200

    Finish CompanyListingScreen

--- app/src/main/java/com/seif/stockmarketapp/presentation/companies_list/CompanyListingScreen.kt
+++ app/src/main/java/com/seif/stockmarketapp/presentation/companies_list/CompanyListingScreen.kt
@@ -1,10 +1,25 @@
 package com.seif.stockmarketapp.presentation.companies_list
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.seif.stockmarketapp.R
+import com.seif.stockmarketapp.presentation.companies_list.component.CompanyItem
+import com.seif.stockmarketapp.util.UiText
 
 @Composable
 @Destination(start = true)
@@ -16,4 +31,43 @@ fun CompanyListingScreen(
         isRefreshing = viewModel.state.isRefreshing
     )
 
+    val state = viewModel.state
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        OutlinedTextField(
+            value = state.searchQuery,
+            onValueChange = {
+                viewModel.onEvent(
+                    CompanyListingEvent.OnSearchQueryChange(it)
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            placeholder = {
+                Text(text = UiText.StringResource(R.string.search_placholder).asString())
+            },
+            maxLines = 1,
+            singleLine = true
+        )
+        SwipeRefresh(state = swipeRefreshState, onRefresh = {
+            viewModel.onEvent(CompanyListingEvent.Refresh)
+        }) {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.companies.size) { i ->
+                    if (i < state.companies.size) // to prevent last item from having the divider
+                        Divider(modifier = Modifier.padding(horizontal = 16.dp))
+
+                    CompanyItem(
+                        company = state.companies[i],
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                // ToDo: Navigate to details screen
+                            }.padding(16.dp)
+                    )
+                }
+            }
+        }
+    }
 }
