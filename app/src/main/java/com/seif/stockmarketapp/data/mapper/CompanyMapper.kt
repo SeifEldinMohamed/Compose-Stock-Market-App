e84facadcfef5266262a2e07f205113a3a0e256e
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-10-03 13:17:41 +0200

    CompanyInfo Mapper and Models Added

--- app/src/main/java/com/seif/stockmarketapp/data/mapper/CompanyMapper.kt
+++ app/src/main/java/com/seif/stockmarketapp/data/mapper/CompanyMapper.kt
@@ -1,6 +1,8 @@
 package com.seif.stockmarketapp.data.mapper
 
 import com.seif.stockmarketapp.data.local.entity.CompanyListingEntity
+import com.seif.stockmarketapp.data.remote.dto.CompanyInfoDto
+import com.seif.stockmarketapp.domain.model.CompanyInfo
 import com.seif.stockmarketapp.domain.model.CompanyListing
 
 fun CompanyListingEntity.toCompanyListing(): CompanyListing {
@@ -17,4 +19,16 @@ fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
         symbol = symbol,
         exchange = exchange
     )
+}
+
+fun CompanyInfoDto.toCompanyInfo(): CompanyInfo {
+    return CompanyInfo(
+        symbol = symbol ?: "",
+        assetType = assetType ?: "",
+        description = description ?: "",
+        name = name ?: "",
+        country = country ?: "",
+        industry = industry ?: "",
+        sector = sector ?: ""
+    )// the fields may be null if we finish our free quota
 }
---
+++ app/src/main/java/com/seif/stockmarketapp/data/remote/dto/IntraDayInfoDto.kt
@@ -0,0 +1,6 @@
+package com.seif.stockmarketapp.data.remote.dto
+
+data class IntraDayInfoDto(
+    val timestamp: String,
+    val close: Double
+)
